2a7d63de9b74bb7f3d57db93d58e492c199f943c
Sullivan677 <decarli.sulli@gmail.com>
2022-06-16 10:15:15 +0200

    Initial Commit

---
+++ Brastlemark/BrastlemarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrastlemarkApp.swift
+//  Brastlemark
+//
+//  Created by Sullivan De carli on 16/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct BrastlemarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
